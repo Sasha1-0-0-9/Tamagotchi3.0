3af75292b489fa548a59e5387961a0bf7d472de4
Sasha <sashapysmenny@gmail.com>
2020-11-27 14:08:05 +0200

    bug fixes

--- Session.rb
+++ Session.rb
@@ -4,26 +4,26 @@ require_relative 'superadmin.rb'
 
 class Session
 
-	def initialize(login, password)
-		@login = login
-		@password = password
-	end
+  def initialize(login, password)
+  	@login = login
+  	@password = password
+  end
 
-	def superadmin?
-		@login == 'super' and @password == '111'
-	end
+  def superadmin?
+  	@login == 'super' and @password == '111'
+  end
 
-	def admin?
-		@login == 'admin' and @password == '111'
-	end
+  def admin?
+  	@login == 'admin' and @password == '111'
+  end
 
-	def log_in
-		if superadmin?
-			user = Superadmin.new(@login, @password)
-		elsif admin?
-			user = Admin.new(@login, @password)
-		else
-			user = User.new(@login, @password)
-		end
-	end
+  def log_in
+  	if superadmin?
+  		user = Superadmin.new(@login, @password)
+  	elsif admin?
+  		user = Admin.new(@login, @password)
+  	else
+  		user = User.new(@login, @password)
+  	end
+  end
 end
