10208ed9975d583ed8662f725d0d98da94fd56c1
Sasha <sashapysmenny@gmail.com>
2020-11-27 13:38:51 +0200

    bug fix

--- superadmin.rb
+++ superadmin.rb
@@ -15,71 +15,71 @@ class Superadmin < User
   end
 
   def commands(pet)
-  	x = gets.chomp.to_s
-  	while x != "0"
-  		case x
-  		when "1"
-  			pet.go_to_sleep
-  			puts pet.show_stats
-  			x = gets.chomp.to_s
-  		when "2"
-  			pet.feed
-  			puts pet.show_stats
-  			x = gets.chomp.to_s
-  		when "3"
-  			pet.walk
-  			puts pet.show_stats
-  			x = gets.chomp.to_s
-  		when "4"
-  			pet.take_a_shower
-  			puts pet.show_stats
-  			x = gets.chomp.to_s
-  		when "5"
-  			pet.heal
-  			puts pet.show_stats
-  			x = gets.chomp.to_s
-  		when "6"
-  			pet.drink_an_energy_drink
-  			puts pet.show_stats
-  			x = gets.chomp.to_s
-  		when "7"
-  			pet.play_boardgames
-  			puts pet.show_stats
-  			x = gets.chomp.to_s
-  		when "8"
-  			pet.spectate
-  			puts pet.show_stats
-  			x = gets.chomp.to_s
-  		when "9"
-  			pet.skip_day
-  			puts pet.show_stats
-  			x = gets.chomp.to_s
-  		when "new_name"
-  			pet.change_name
-  			x = gets.chomp.to_s
-  		when "change_state"
-  			pet.change_values
-  			x = gets.chomp.to_s
-  		when "kill"
-  			pet.kill
-  			x = gets.chomp.to_s
-  		when "reset"
-  			pet.reset
-  			puts pet.show_stats
-  			x = gets.chomp.to_s
-  		when "new_login"
-  			pet.change_login
-  			x = gets.chomp.to_s
+    x = gets.chomp.to_s
+    while x != "0"
+      case x
+      when "1"
+        pet.go_to_sleep
+        puts pet.show_stats
+        x = gets.chomp.to_s
+      when "2"
+        pet.feed
+        puts pet.show_stats
+        x = gets.chomp.to_s
+      when "3"
+        pet.walk
+        puts pet.show_stats
+        x = gets.chomp.to_s
+      when "4"
+        pet.take_a_shower
+        puts pet.show_stats
+        x = gets.chomp.to_s
+      when "5"
+        pet.heal
+        puts pet.show_stats
+        x = gets.chomp.to_s
+      when "6"
+        pet.drink_an_energy_drink
+        puts pet.show_stats
+        x = gets.chomp.to_s
+      when "7"
+        pet.play_boardgames
+        puts pet.show_stats
+        x = gets.chomp.to_s
+      when "8"
+        pet.spectate
+        puts pet.show_stats
+        x = gets.chomp.to_s
+      when "9"
+        pet.skip_day
+        puts pet.show_stats
+        x = gets.chomp.to_s
+      when "new_name"
+        pet.change_name
+        x = gets.chomp.to_s
+      when "change_state"
+        pet.change_values
+        x = gets.chomp.to_s
+      when "kill"
+        pet.kill
+        x = gets.chomp.to_s
+      when "reset"
+        pet.reset
+        puts pet.show_stats
+        x = gets.chomp.to_s
+      when "new_login"
+        pet.change_login
+        x = gets.chomp.to_s
       when "HESOYAM"
         pet.add_money
         x = gets.chomp.to_s
       when  "help"
-  			puts pet.help
-  			x = gets.chomp.to_s
-  		else
-  			p "ERROR. Use 'help' command to get more info"
-  			x = gets.chomp.to_s
-  		end
-  	end
+        puts pet.help
+        x = gets.chomp.to_s
+      else
+        p "ERROR. Use 'help' command to get more info"
+        x = gets.chomp.to_s
+      end
+    end
   end
 end
